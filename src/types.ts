722fd086eed6718b891ad5af186e8406c1df4b60
Abdulhamid <zoubi.abdulhamid@gmail.com>
2022-10-07 23:45:13 +0200

    feat (LocatorMission): new model & state

--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,16 @@ export interface LocatorBlock {
 
   note: string
 }
+
+export interface LocatorMission {
+  title: string
+
+  startPoint: [string, string]
+  endPoint: [string, string]
+
+  matrix: LocatorBlock[][]
+  currentBlockIndex: {
+    row: number 
+    col: number
+  }
+}
